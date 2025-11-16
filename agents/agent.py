f069683245afa2b377a2662ffd123ca25bbb14ce
Sun Kim <sunkim924@gmail.com>
2025-11-16 11:30:00 -0500

    give agent mapbox tools

--- agents/agent.py
+++ agents/agent.py
@@ -3,6 +3,8 @@ from typing import Tuple, List, Dict
 from strands import Agent
 from strands.models.litellm import LiteLLMModel
 from strands.tools.executors import SequentialToolExecutor
+from strands.tools.mcp import MCPClient
+from mcp.client.streamable_http import streamablehttp_client
 from bedrock_agentcore.runtime import BedrockAgentCoreApp
 from tools import fuel_price_assistant, geocode_location
 from prompts import SYSTEM_PROMPT
@@ -21,25 +23,41 @@ model = LiteLLMModel(
     model_id="openai/gpt-5-nano"
 )
 
+# to debug agents
+def debugger_callback_handler(**kwargs):
+    # Print the values in kwargs so that we can see everything
+    print(kwargs)
+
+
 @app.entrypoint
 def invoke_agent(payload: Dict):
 
-    # define our agent
-    agent = Agent(
-        model=model,
-        system_prompt=SYSTEM_PROMPT,
-        tool_executor=SequentialToolExecutor(),
-        tools=[
-            geocode_location,
-            fuel_price_assistant
-        ]
+    streamable_http_mcp_client = MCPClient(
+        lambda: streamablehttp_client(
+            url="https://mcp.mapbox.com/mcp",
+            headers={"Authorization": f"Bearer {os.getenv('MAPBOX_ACCESS_TOKEN')}"}
+        )
     )
+    with streamable_http_mcp_client:
+        mapbox_tools = streamable_http_mcp_client.list_tools_sync()
+        # define our agent
+        agent = Agent(
+            model=model,
+            system_prompt=SYSTEM_PROMPT,
+            tools=[
+                geocode_location,
+                mapbox_tools,
+                fuel_price_assistant
+            ]
+            # callback_handler=debugger_callback_handler,
+            # tool_executor=SequentialToolExecutor(),
+        )
 
-    user_input = payload.get("prompt")
-    print(f"User query: '{user_input}'")
+        user_input = payload.get("prompt")
+        print(f"User query: '{user_input}'")
 
-    result = agent(user_input)
-    return {"result": result.message}
+        result = agent(user_input)
+        return {"result": result.message}
     
 
 if __name__ == "__main__":
--- agents/prompts.py
+++ agents/prompts.py
@@ -20,7 +20,17 @@ First, if the user has not provided the location, you will ask for the user's lo
 Once user has provided the location, you will use the user input address/postcode to get a postcode, latitude and longitude of the user location using 'geocode_location' tool.
 
 Second, given the user's location, you will use a specialized agent called 'fuel_price_assistant'. 
-You will direct any queries about fuel prices in NSW to the 'fuel_price_assistant'
+You will direct any queries about fuel prices in NSW to the 'fuel_price_assistant'.
+
+You will also have tools from Mapbox available to you. Use the following tools from Mapbox to handle queries relating to driving, directions, or maps:
+- 'category_search_tool': Searches for geographic categories such as countries, regions, or postal codes. Useful for narrowing down search queries to a specific type of place.
+- 'directions_tool': Calculates optimal routes between waypoints for driving, walking, or cycling.
+- 'isochrone_tool': Calculates areas reachable within a specified travel time from a location.
+- 'matrix_tool': Computes travel times and distances between multiple points. Ideal for optimizing logistics
+- 'poi_search_tool': Finds points of interest such as restaurants, gas stations, or landmarks near a given location.
+- 'reverse_geocode_tool': Converts geographic coordinates into a readable address or place name.
+- 'static_map_tool': Provides static map images of a specified location and zoom level with marker, circle, line, and polygon data overlays. 
+
 
 You will give helpful information about fuel prices given the user's location.
 """
@@ -33,4 +43,17 @@ You will use any of the following tools to answer user's query as many times as 
 - 'get_prices_for_location':  Returns current fuel prices for a single fuel type, station brands, and a named location (postcode).
 - 'get_nearby_prices':  Returns fuel prices for multiple fuel stations within a specified radius of a location.
 - 'get_price_at_station': Retrieve the current fuel prices for a single station by station code.
+
+For 'fueltype' parameter in any of the tools above, use the following mapping:
+- 'E10' for Ethanol 94
+- 'U91' for Unleaded 91 
+- 'E85' for Ethanol 105
+- 'P95' for Premium 95
+- 'P98' for Premium 98
+- 'DL' for Diesel
+- 'PDL' for Premium Diesel
+- 'B20' for Biodiesel 20
+- 'LPG' for LPG
+- 'CNG' for 'CNG/NGV
+- 'EV' for Electric vehicle charge
 """
