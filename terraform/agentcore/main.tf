01113e2d8cfff3cc88b1f78fbe4979af08e310ed
Sun Kim <sunkim924@gmail.com>
2026-04-05 01:16:42 -0400

    add all

--- terraform/agentcore/main.tf
+++ terraform/agentcore/main.tf
@@ -7,3 +7,29 @@ resource "aws_bedrockagentcore_memory" "nsw_fuel_agent_memory" {
   name                  = "nsw_fuel_agent_memory"
   event_expiry_duration = 7
 }
+
+data "aws_iam_role" "bedrock_runtime" {
+  name = "AmazonBedrockAgentCoreSDKRuntime-us-east-1-9245b9760a"
+}
+
+resource "aws_iam_role_policy" "bedrock_agentcore_memory_access" {
+  name = "BedrockAgentCoreMemoryAccessPolicy"
+  role = data.aws_iam_role.bedrock_runtime.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "bedrock-agentcore:ListMemories",
+          "bedrock-agentcore:GetMemory",
+          "bedrock-agentcore:CreateMemory",
+          "bedrock-agentcore:UpdateMemory",
+          "bedrock-agentcore:DeleteMemory"
+        ]
+        Resource = "arn:aws:bedrock-agentcore:us-east-1:794944452292:memory/*"
+      }
+    ]
+  })
+}
